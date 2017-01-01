b2391ed0307265ee0c5a9d92cf74709f4f7e3b30
adam <adavenpo@purdue.edu>
2017-01-01 15:50:12 -0500

    Creating models folder and adding mongoose model for campgrounds

---
+++ models/campground.js
@@ -0,0 +1,8 @@
+// Campground Schema
+var mongoose = require("mongoose");
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+module.exports = mongoose.model("Campground", campgroundSchema);
