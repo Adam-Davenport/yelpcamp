63bc65798e9c02bee88b7c5597834b5842637756
adam <adavenpo@purdue.edu>
2017-01-02 17:38:14 -0500

    Comment association added

--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,14 @@ var mongoose = require("mongoose");
 var campgroundSchema = new mongoose.Schema({
 	name: String,
 	image: String,
-	description: String
+	description: String,
+	// Storing association on the campground will be refactored to normal db relationship later
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Comment"
+		}
+	]
 });
 // Export the campgroundSchema as a model
 module.exports = mongoose.model("Campground", campgroundSchema);
