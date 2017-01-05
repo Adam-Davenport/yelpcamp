8be8578d0b9da4020b1673122b043f08881336af
Adam Davenport <adavenpo@purdue.edu>
2017-01-05 01:26:11 -0500

    Adding comments to better doucment what is going on

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Initial required modules
 var express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
@@ -5,8 +6,12 @@ var express = require("express"),
 	Campground = require("./models/campground"),
 	seedDB = require("./seeds");
 
+// Connect to database
 mongoose.connect("mongodb://localhost/yelp_camp");
+
+// Use body-parser middleware for getting information from post and put requests
 app.use(bodyParser.urlencoded({extended: true}));
+
 // Serve static files from the public folder
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -15,17 +20,17 @@ seedDB();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-	res.render("landing", {title:"YelpCamp"});
+	res.render("landing", {title: "YelpCamp"});
 });
 
 // Finding a campground
-function findCampground(id){
+function findCampground(id) {
 	return Campground.findById(id, findItem);
 }
 
 // Can be used with any mongoose search
-function findItem(error, item){
-	if(error){
+function findItem(error, item) {
+	if (error) {
 		console.log(error);
 		return null;
 	}
@@ -34,49 +39,52 @@ function findItem(error, item){
 	}
 }
 
-
 //=====================
 // Campground Routes
 //=====================
+
+// Index Route
 app.get("/campgrounds", function (req, res) {
 	// Get all campgrounds from DB
 	Campground.find({}, function (error, campgrounds) {
-		if(error){
+		if (error) {
 			console.log(error);
 		}
-		else{
-			res.render("campgrounds/index", {title:"Campgrounds", campgrounds:campgrounds});
+		else {
+			res.render("campgrounds/index", {title: "Campgrounds", campgrounds: campgrounds});
 		}
-		});
+	});
 });
 
+// Create route
 app.post("/campgrounds", function (req, res) {
 	var camp = req.body.camp;
 	var image = req.body.image;
 	var description = req.body.description;
-	var newCampground = {name:camp, image:image, description:description};
+	var newCampground = {name: camp, image: image, description: description};
 	// Create a new campground and save to DB
 	Campground.create(newCampground, function (error) {
-		if(error){
+		if (error) {
 			console.log(error);
 		}
-		else{
+		else {
 			res.redirect('/campgrounds');
 		}
 	});
 });
 
+// New Route
 app.get("/campgrounds/new", function (req, res) {
-	res.render("campgrounds/new.ejs", {title:"Submit Campground"});
+	res.render("campgrounds/new.ejs", {title: "Submit Campground"});
 });
 
-// Show route for campground must be declared after new or other routes directly under /campgrouds
-app.get("/campgrounds/:id", function (req ,res) {
+// Show route for campground must be declared after new or other routes directly under campgrounds directory
+app.get("/campgrounds/:id", function (req, res) {
 	Campground.findById(req.params.id).populate("comments").exec(function (error, foundCampground) {
-		if(error){
+		if (error) {
 			console.log(error);
 		}
-		else{
+		else {
 			res.render("campgrounds/show", {title: foundCampground.name, campground: foundCampground});
 		}
 	});
@@ -86,29 +94,32 @@ app.get("/campgrounds/:id", function (req ,res) {
 //======================================
 //   Camground comment routes
 //======================================
-app.get("/campgrounds/:id/comments/new", function(req, res){
+// New route
+app.get("/campgrounds/:id/comments/new", function (req, res) {
 	// var campground = findCampground(req.params.id);
 	Campground.findById(req.params.id, function (error, campground) {
-		if(error){
+		if (error) {
 			console.log(error);
 		}
-		else{
+		else {
 			res.render("comments/new", {title: "Comment", id: req.params.id, campground: campground});
 		}
 	});
 });
 
+// Create route
 app.post("/campgrounds/:id/comments", function (req, res) {
 	var campground = findCampground(req.params.id);
-	if(campground){
+	if (campground) {
 		res.redirect("/campgrounds/" + req.params.id);
 	}
-	else{
+	else {
 		res.send("Campground not found");
 	}
 });
 //=================================================
 
-app.listen(3000, function() {
+// Set the app to listen on port 3000
+app.listen(3000, function () {
 	console.log("The yelpcamp server has started.");
 });
