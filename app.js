2de5dd56e2510a59b483c4177267ca648266183e
adam <adavenpo@purdue.edu>
2017-01-10 23:00:36 -0500

    Implementing passport authentificaiton to the system

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // Initial required modules
 var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose'),
-	Campground = require('./models/campground'),
-	Comment = require('./models/comment')
-	seedDB = require('./seeds')
+	app           = express(),
+	seedDB        = require('./seeds'),
+	passport      = require('passport'),
+	mongoose      = require('mongoose'),
+	bodyParser    = require('body-parser'),
+	User          = require('./models/user'),
+	LocalStrategy = require('passport-local'),
+	Comment       = require('./models/comment'),
+	Campground    = require('./models/campground')
+
+//=======================
+// Database Config
+//=======================
 
 // Connect to database
 mongoose.connect('mongodb://localhost/yelp_camp')
+
+// Run the seed DB to remove and repopulate the database
+seedDB()
 
 // Use body-parser middleware for getting information from post and put requests
 app.use(bodyParser.urlencoded({extended: true}))
@@ -16,29 +26,31 @@ app.use(bodyParser.urlencoded({extended: true}))
 // Serve static files from the public folder
 app.use('/public', express.static(__dirname + '/public'))
 
-// Run the seed DB to remove and repopulate the database
-seedDB()
+// Set the templating engine to EJS
 app.set('view engine', 'ejs')
 
+// Passport Configuration
+app.use(require('express-session')({
+	secret: "This is a message for authentication of the system",
+	resave: false,
+	saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+//=========================
+//         ROUTES
+//=========================
+
+// Root Route
 app.get('/', function (req, res) {
 	res.render('landing', {title: 'YelpCamp'})
 })
-
-// Finding a campground
-function findCampground(id) {
-	return Campground.findById(id, findItem)
-}
-
-// Can be used with any mongoose search
-function findItem(error, item) {
-	if (error) {
-		console.log(error)
-		return null
-	}
-	else {
-		return item
-	}
-}
 
 //=====================
 // Campground Routes
@@ -132,6 +144,30 @@ app.post('/campgrounds/:id/comments', function (req, res) {
 		}
 	})
 })
+
+//========================
+// Authentication Routes
+//========================
+
+// Show register form
+app.get('/register', function(req,res){
+	res.render('register')
+})
+
+// Handle Signup logic
+app.post('/register', function(req, res){
+	var newUser = new User({username: req.body.username})
+	User.register(newUser, req.body.password, function(error, user){
+		if(error){
+			console.log(error)
+			return res.render('register')
+		}
+		passport.authenticate('local')(req,res, function (){
+			res.redirect('/campgrounds')
+		})
+	})
+})
+
 //=================================================
 
 // Set the app to listen on port 3000
