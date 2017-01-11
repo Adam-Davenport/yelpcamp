d7b40e1f85e43837d0c8a55eefd1e36116c5da0f
adam <adavenpo@purdue.edu>
2017-01-10 22:50:29 -0500

    Implemented the mongoose model for users

---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+		plm = require('passport-local-mongoose'),
+
+//  Define the User Schema
+		userSchema = new mongoose.Schema({
+			username: String,
+			password: String
+		})
+
+// Plug in passport local mongoose to the user schema
+userSchema.plugin(plm)
+
+// Export the model
+module.exports = mongoose.model("User", userSchema)
