53cfe9d5c8ed92bc5b464940e6ec666725b2dace
adam <adavenpo@purdue.edu>
2017-01-10 23:10:39 -0500

    Changed for modern js formatting

--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,9 @@
 // Comment Schema
-var mongoose = require("mongoose");
+var mongoose = require('mongoose')
 // Create the schema for the comment
 var commentSchema = new mongoose.Schema({
 	text: String,
 	author: String
-});
+})
 // Export the schema as a model
-module.exports = mongoose.model("Comment", commentSchema);
+module.exports = mongoose.model('Comment', commentSchema)
