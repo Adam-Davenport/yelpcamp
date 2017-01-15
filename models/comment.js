4bd06e66ed805e85c206c14fd47780fb72494473
adam <adavenpo@purdue.edu>
2017-01-15 15:04:57 -0500

    Adding author to the comment schema

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose')
 // Create the schema for the comment
 var commentSchema = new mongoose.Schema({
 	text: String,
-	author: String
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	}
 })
 // Export the schema as a model
 module.exports = mongoose.model('Comment', commentSchema)
