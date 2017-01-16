ec0cbb94c856cb6341a84584f263cabd931e9825
adam <adavenpo@purdue.edu>
2017-01-15 23:57:08 -0500

    Adding author reference to campground

--- models/campground.js
+++ models/campground.js
@@ -4,6 +4,14 @@ var campgroundSchema = new mongoose.Schema({
 	name: String,
 	image: String,
 	description: String,
+	// Storing the user who created the campground
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	},
 	// Storing association on the campground will be refactored to normal db relationship later
 	comments: [
 		{
