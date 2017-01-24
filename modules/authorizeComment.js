8ed7aba1d904a2d1893ed97b43bf0fb538376208
adam <adavenpo@purdue.edu>
2017-01-24 16:05:59 -0500

    Adding flash messages to authorization

--- modules/authorizeComment.js
+++ modules/authorizeComment.js
@@ -4,6 +4,7 @@ var Comment = require('../models/comment')
 function authorize(req, res, next){
 	Comment.findById(req.params.comment, function (error, foundComment) {
 		if(error){
+			req.flash('error', error.message)
 			res.redirect('back')
 		}
 		else{
@@ -12,6 +13,7 @@ function authorize(req, res, next){
 				next()
 			}
 			else{
+				req.flash('Error', 'You are not authorized to do that operation')
 				res.redirect('back')
 			}
 		}
