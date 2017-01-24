3f5f0c487261352c4d394fa430124002ad91db79
adam <adavenpo@purdue.edu>
2017-01-24 16:04:46 -0500

    Adding flash messages to authorization

--- modules/authorizeCampground.js
+++ modules/authorizeCampground.js
@@ -5,13 +5,15 @@ var	Campground = require('../models/campground')
 function authorize(req, res , next) {
 	Campground.findById(req.params.id, function (error, foundCampground) {
 		if(error){
+			req.flash('error', error.message)
 			res.redirect('back')
 		}
 		else{
 			if(req.user._id.equals(foundCampground.author.id)){
-				next()
+				return next()
 			}
 			else{
+				req.flash('error', 'You are not authorized to do that operation.')
 				res.redirect('back')
 			}
 		}
