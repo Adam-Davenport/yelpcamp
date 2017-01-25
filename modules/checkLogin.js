358e7df14e201a8f81f043363f27a317f714d89d
adam <adavenpo@purdue.edu>
2017-01-24 23:57:03 -0500

    Adding session return url

--- modules/checkLogin.js
+++ modules/checkLogin.js
@@ -4,6 +4,7 @@ function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next()
 	}
+	req.session.returnTo = req.originalUrl
 	req.flash('error', 'Please log in first')
 	res.redirect('/login')
 }
