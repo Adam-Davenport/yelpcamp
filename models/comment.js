56da1fabde54add6f4a53558c794e4aa52a4d75b
Adam-Davenport <adavenpo@purdue.edu>
2017-03-14 16:35:45 -0400

    Adding score to comment model

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
 // Create the schema for the comment
 var commentSchema = new mongoose.Schema({
 	text: String,
+	score: Number,
 	author: {
 		id: {
 			type: mongoose.Schema.Types.ObjectId,
